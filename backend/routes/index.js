e9da5f85d16bb1dcbfc2a73474fcd489ba94d458
huanglallen <huanglallen@gmail.com>
2023-04-13 13:12:47 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-//setup testing//
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
@@ -14,5 +8,9 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
+
 
 module.exports = router;
