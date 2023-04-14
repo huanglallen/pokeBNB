ca10225d3bd39bba7a14554a48bd2b72f00fb41c
huanglallen <huanglallen@gmail.com>
2023-04-14 13:04:06 -0700

    Add User input validation on user signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,38 +10,6 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-// Log in
-router.post('/', async (req, res, next) => {
-    const { credential, password } = req.body;
-
-    const user = await User.unscoped().findOne({
-        where: {
-            [Op.or]: {
-            username: credential,
-            email: credential
-            }
-        }
-    });
-
-    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = { credential: 'The provided credentials were invalid.' };
-            return next(err);
-    }
-
-    const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-    };
-    await setTokenCookie(res, safeUser);
-
-    return res.json({user: safeUser});
-    }
-);
-
 // Log out
 router.delete('/',(_req, res) => {
     res.clearCookie('token');
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -3,8 +3,8 @@ const { validationResult } = require('express-validator');
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
-  const validationErrors = validationResult(req);
-
+    const validationErrors = validationResult(req);
+    console.log('RIGHT HERE')
   if (!validationErrors.isEmpty()) {
     const errors = {};
     validationErrors
