6f9412b386595ae2dcd00fc4abdc2e6b8896df49
huanglallen <huanglallen@gmail.com>
2023-04-18 16:27:19 -0700

    get all spots completed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,8 +3,48 @@ const router = express.Router();
 
 const { Spot, SpotImage, Review } = require('../../db/models');
 
+router.get('/:spotId', async (req, res) => {
+    const { spotId }= req.params;
+    const spots = await Spot.findAll({
+        include: [
+            { model: Review },
+            { model: SpotImage }
+        ]
+    });
+    //get all spots in js format
+    let spotsList = [];
+    spots.forEach(spot => {
+        spotsList.push(spot.toJSON())
+    });
+
+    //create avgRating
+    spotsList.forEach(spot => {
+        let sum = 0;
+        for(let i = 0; i < spot.Reviews.length; i++) {
+            sum += spot.Reviews[i].stars
+        }
+        spot.avgRating = sum / spot.Reviews.length;;
+        delete spot.Reviews;
+    })
+
+    //create previewImage
+    spotsList.forEach(spot => {
+            spot.SpotImages.forEach(image => {
+                if(image.url) {
+                    spot.previewImage = image.url;
+                }
+            })
+        if(!spot.previewImage) {
+            spot.previewImage = 'no image found';
+        };
+        delete spot.SpotImages;
+    });
+    const result = {Spots: spotsList};
+
+    return res.json(result);
+})
+
 router.get('/', async (req, res) => {
-    console.log('route hit')
     const spots = await Spot.findAll({
         include: [
             { model: Review },
@@ -40,7 +80,9 @@ router.get('/', async (req, res) => {
         delete spot.SpotImages;
     });
 
-    return res.json(spotsList);
+    const result = {Spots: spotsList};
+
+    return res.json(result);
 })
 
 module.exports = router;
