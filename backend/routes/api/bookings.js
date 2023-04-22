313241a9b4bd6b866a543cd0e9af7212a8162304
huanglallen <huanglallen@gmail.com>
2023-04-22 02:00:15 -0700

    delete bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -25,12 +25,14 @@ router.get('/current', requireAuth, async (req, res) => {
 
     //add previewImage
     bookingsList.forEach((booking) => {
-        booking.Spot.previewImage = 'none';
-        const prevImg = booking.Spot.SpotImages.find((el) => el.preview === true);
-        if (prevImg) {
-          booking.Spot.previewImage = prevImg.url;
-        };
-        delete booking.Spot.SpotImages;
+        if(booking.Spot) {
+            booking.Spot.previewImage = 'none';
+            const prevImg = booking.Spot.SpotImages.find((el) => el.preview === true);
+            if (prevImg) {
+              booking.Spot.previewImage = prevImg.url;
+            };
+            delete booking.Spot.SpotImages;
+        }
       });
 
     //reposition columns
@@ -46,6 +48,43 @@ router.get('/current', requireAuth, async (req, res) => {
     }));
 
     return res.json({ Bookings: result });
+});
+
+// router.put('/:bookingId', requireAuth, async (req, res) => {
+
+// });
+
+router.delete('/:bookingId', requireAuth, async (req, res) => {
+    const { bookingId } = req.params;
+    const user = req.user.id;
+    const booking = await Booking.findByPk(bookingId);
+
+    //if booking is missing
+    if(!booking) {
+        return res.status(404).json({
+            message: "Booking couldn't be found"
+        })
+    }
+
+    //validate if user
+    if(booking.userId !== user || booking.userId !== user) {
+        return res.status(403).json({
+            message: "Booking must belong to the current user or the Spot must belong to the current user"
+        });
+    };
+
+    // Check if booking has already started
+    const currTime = new Date().getTime();
+    const bookingStartDate = new Date(booking.startDate).getTime();
+
+    if (bookingStartDate <= currTime) {
+        return res.status(403).json({
+            message: "Bookings that have been started can't be deleted"
+        });
+    }
+
+    await booking.destroy();
+    return res.json({ message: "Successfully deleted" });
 });
 
 module.exports = router;
