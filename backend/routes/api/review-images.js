843e5e127213c73c8b0c3362a8fa88e2dd7d5b0d
huanglallen <huanglallen@gmail.com>
2023-04-22 03:09:43 -0700

    delete reviewImages

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -3,31 +3,31 @@ const router = express.Router();
 
 const { requireAuth } = require('../../utils/auth');
 
-const { Spot, SpotImage } = require('../../db/models');
+const { User, Spot, SpotImage, Review, ReviewImage, Booking } = require('../../db/models');
 
 router.delete('/:imageId', requireAuth, async (req, res) => {
     const { imageId } = req.params;
     const user = req.user.id;
-    const img = await SpotImage.findByPk(imageId);
+    const img = await ReviewImage.findByPk(imageId);
 
-    //if spotImage couldn't be found
+    //if reviewImage couldn't be found
     if(!img) {
         res.status(404).json({
-            message: "Spot Image couldn't be found"
+            message: "Review Image couldn't be found"
         })
     }
 
     //img must belong to current user
-    const spot = await Spot.findByPk(img.spotId);
-    if(user !== spot.ownerId) {
+    const review = await Review.findByPk(img.reviewId);
+    if(user !== review.userId) {
         return res.status(403).json({
-            message: "Only the user can delete their spotImage"
+            message: "Only the user can delete their reviewImage"
         });
-    }
+    };
 
     await img.destroy();
 
     return res.json({ message: "successfully deleted" });
-})
+});
 
 module.exports = router;
