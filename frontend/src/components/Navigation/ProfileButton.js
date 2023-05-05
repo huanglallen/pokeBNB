26f984d15bf58fbde5209ac4e22af72649d44d6a
huanglallen <huanglallen@gmail.com>
2023-05-05 15:14:40 -0700

    fontawesome icon added

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,9 @@
+const ProfileButton = () => {
+    return (
+        <div style={{ fontSize: "100px" }}>
+            <i className="fa-solid fa-user"></i>
+        </div>
+    )
+}
+
+export default ProfileButton;
