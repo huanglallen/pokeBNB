07ec36a1acca4887390ed5fce002ad0c2505055a
huanglallen <huanglallen@gmail.com>
2023-05-05 15:33:23 -0700

    move logout logic into ProfileButton

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,34 @@
-const ProfileButton = () => {
-    return (
-        <div style={{ fontSize: "100px" }}>
-            <i className="fa-solid fa-user"></i>
-        </div>
-    )
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown";
+
+  return (
+    <>
+        <button>
+            <div style={{ fontSize: "100px" }}>
+                <i className="fa-solid fa-user"></i>
+            </div>
+        </button>
+        <ul className="profile-dropdown">
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+                <button onClick={logout}>Log Out</button>
+            </li>
+        </ul>
+    </>
+  );
 }
 
 export default ProfileButton;
