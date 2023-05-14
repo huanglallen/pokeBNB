b0faaf45df32ff24203a61064f8d2b971225f3fd
huanglallen <huanglallen@gmail.com>
2023-05-14 15:32:33 -0700

    deleteSpot modal added

--- frontend/src/components/SpotDeleteModal/index.js
+++ frontend/src/components/SpotDeleteModal/index.js
@@ -1,18 +1,47 @@
+// import React from 'react';
+// import './SpotDeleteModal.css';
+// import { useDispatch } from 'react-redux';
+// import { deleteSpot } from '../../store/spots';
+
+// const SpotDeleteModal = ({ spot, setShowDeleteModal }) => {
+//   const dispatch = useDispatch();
+//   console.log('')
+
+//   const handleDelete = () => {
+//     dispatch(deleteSpot(spot.id));
+//     setShowDeleteModal(false);
+//   };
+
+//   const handleCancel = () => {
+//     setShowDeleteModal(false);
+//   };
+
+//   return (
+//     <div id="DeleteModalWrapper">
+//       <h2>Confirm Delete</h2>
+//       <h3>Are you sure you want to remove this spot from the listings?</h3>
+//       <button onClick={handleDelete}>Yes (Delete Spot)</button>
+//       <button onClick={handleCancel}>No (Keep Spot)</button>
+//     </div>
+//   );
+// };
+
+// export default SpotDeleteModal;
+
 import React from 'react';
 import './SpotDeleteModal.css';
-import { useDispatch } from 'react-redux';
-import { deleteSpot } from '../../store/spots';
+import { useModal } from '../../context/Modal';
 
-const SpotDeleteModal = ({ spot, setShowDeleteModal }) => {
-  const dispatch = useDispatch();
+const SpotDeleteModal = ({ spot, handleDeleteConfirmation }) => {
+  const { closeModal } = useModal();
 
   const handleDelete = () => {
-    dispatch(deleteSpot(spot.id));
-    setShowDeleteModal(false);
+    handleDeleteConfirmation();
+    closeModal();
   };
 
   const handleCancel = () => {
-    setShowDeleteModal(false);
+    closeModal();
   };
 
   return (
@@ -26,4 +55,3 @@ const SpotDeleteModal = ({ spot, setShowDeleteModal }) => {
 };
 
 export default SpotDeleteModal;
-
--- frontend/src/components/SpotIndexItem/SpotIndexItem.js
+++ frontend/src/components/SpotIndexItem/SpotIndexItem.js
@@ -1,11 +1,91 @@
+// import { Link, useLocation } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useState } from "react";
+// import { deleteSpot } from "../../store/spots";
+// import { Modal } from "../../context/Modal";
+// import SpotDeleteModal from "../SpotDeleteModal";
+// import "./SpotIndexItem.css";
+
+
+// const SpotIndexItem = ({ spot }) => {
+//   const dispatch = useDispatch();
+//   const location = useLocation();
+//   const rating = spot.avgRating;
+//   const spotImg = spot.previewImage;
+//   const currentUser = useSelector((state) => state.session.user);
+
+//   const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+//   const handleDelete = (e) => {
+//     e.preventDefault();
+//     // dispatch(deleteSpot(spot.id))
+//     setShowDeleteModal(true);
+//   };
+
+//   const handleDeleteConfirmation = () => {
+//     dispatch(deleteSpot(spot.id));
+//     setShowDeleteModal(false);
+//   };
+
+//   const handleDeleteCancel = () => {
+//     setShowDeleteModal(false);
+//   };
+
+//   if (!spotImg) return null;
+
+//   return (
+//     <li>
+//       <div className="spot">
+//         <Link to={`/spots/${spot.id}`}>
+//           <div className="img">
+//             <img className="spotImg" src={spotImg} alt={`${spot.name}`} />
+//           </div>
+//           <div className="spotLocation">
+//             {spot.city}, {spot.state}
+//           </div>
+//           <div className="spotRating">
+//             <i className="fa-solid fa-star"></i>
+//             {rating ? rating : "New"}
+//           </div>
+//           <div className="spotPrice">${spot.price} night</div>
+//         </Link>
+//         {currentUser &&
+//           location.pathname === "/spots/current" &&
+//           currentUser.id === spot.ownerId && (
+//             <div className="spotButtons">
+//               <Link exact to={`/spots/${spot.id}/edit`} className="update">
+//                 Update
+//               </Link>
+//               <button className="delete" onClick={handleDelete}>
+//                 Delete
+//               </button>
+//             </div>
+//           )}
+//       </div>
+//       {showDeleteModal && (
+//         <Modal onClose={() => setShowDeleteModal(false)}>
+//           <div id="DeleteModalWrapper">
+//             <h2>Confirm Delete</h2>
+//             <h3>Are you sure you want to remove this spot from the listings?</h3>
+//             <button onClick={handleDeleteConfirmation}>
+//               Yes (Delete Spot)
+//             </button>
+//             <button onClick={handleDeleteCancel}>No (Keep Spot)</button>
+//           </div>
+//         </Modal>
+//       )}
+//     </li>
+//   );
+// };
+
+// export default SpotIndexItem;
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { deleteSpot } from "../../store/spots";
-import { Modal } from "../../context/Modal";
+import { Modal, useModal } from "../../context/Modal";
 import SpotDeleteModal from "../SpotDeleteModal";
 import "./SpotIndexItem.css";
-
 
 const SpotIndexItem = ({ spot }) => {
   const dispatch = useDispatch();
@@ -14,21 +94,29 @@ const SpotIndexItem = ({ spot }) => {
   const spotImg = spot.previewImage;
   const currentUser = useSelector((state) => state.session.user);
 
+  const { setModalContent, closeModal } = useModal();
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   const handleDelete = (e) => {
     e.preventDefault();
-    // dispatch(deleteSpot(spot.id))
+    setModalContent(
+      <SpotDeleteModal spot={spot}
+        handleDeleteConfirmation={handleDeleteConfirmation}
+        handleDeleteCancel={handleDeleteCancel}
+      />
+    );
     setShowDeleteModal(true);
   };
 
   const handleDeleteConfirmation = () => {
     dispatch(deleteSpot(spot.id));
     setShowDeleteModal(false);
+    closeModal();
   };
 
   const handleDeleteCancel = () => {
     setShowDeleteModal(false);
+    closeModal();
   };
 
   if (!spotImg) return null;
@@ -62,18 +150,6 @@ const SpotIndexItem = ({ spot }) => {
             </div>
           )}
       </div>
-      {showDeleteModal && (
-        <Modal onClose={() => setShowDeleteModal(false)}>
-          <div id="DeleteModalWrapper">
-            <h2>Confirm Delete</h2>
-            <h3>Are you sure you want to remove this spot from the listings?</h3>
-            <button onClick={handleDeleteConfirmation}>
-              Yes (Delete Spot)
-            </button>
-            <button onClick={handleDeleteCancel}>No (Keep Spot)</button>
-          </div>
-        </Modal>
-      )}
     </li>
   );
 };
