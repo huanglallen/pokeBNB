2106f9f73c62dc33776979e65f83ec26aa01a72a
huanglallen <huanglallen@gmail.com>
2023-05-15 02:47:59 -0700

    spotDetails done

--- frontend/src/components/SpotShow/SpotShow.js
+++ frontend/src/components/SpotShow/SpotShow.js
@@ -7,6 +7,7 @@ import { getReviews } from "../../store/reviews";
 import OpenModalButton from "../OpenModalButton";
 import { useModal } from "../../context/Modal";
 import CreateReviewForm from "../CreateReview";
+import './SpotShow.css';
 
 const SpotShow = () => {
     const dispatch = useDispatch();
@@ -15,6 +16,7 @@ const SpotShow = () => {
     const spot = useSelector(state => state.spots.singleSpot);
     const user = useSelector(state => state.session.user);
     const reviews = useSelector(state => state.reviews.user);
+    const rating = useSelector(state => state.spots.singleSpot.avgStarRating)
     // const allReviewUsers = useSelector(state => state.reviews.user);
     // const existingReview = Object.values(allReviewUsers).some(user => user.User.userData.id);
     // console.log('SPOT_allReviews', allReviewUsers)
@@ -45,41 +47,60 @@ const SpotShow = () => {
 
     return (
         <div className="spotShow">
-            <h2>{spot.name}</h2>
-            <h3>
+            <h2 className="spotname">{spot.name}</h2>
+            <h3 className="location">
                 {spot.city}, {spot.state}, {spot.country}
             </h3>
             <div className="showImgs">
-            <div className="prevImg">
+                <div className="prevImg">
                     {spot.SpotImages.slice(0, 1).map(renderImage)}
                 </div>
                 <div className="otherImgs">
-                {spot.SpotImages.slice(1, 5).map(renderImage)}
+                    {spot.SpotImages.slice(1, 5).map(renderImage)}
                 </div>
             </div>
             <div className="info">
                 <div className="spotContext">
-                    <h2>Hosted by {spot.Owner && `${spot.Owner.firstName} ${spot.Owner.lastName}`}</h2>
-                    <p>{spot.description}</p>
+                    <h2 className="host">Hosted by {spot.Owner && `${spot.Owner.firstName} ${spot.Owner.lastName}`}</h2>
+                    <p className="spotdescription">{spot.description}</p>
                 </div>
-                <div className="reserve">
-                    <div className="reserveInfo">
-                        {`$${spot.price} night`}
+                <div className="reserveWrapper">
+                    <div className="reserve">
+                        <div className="reserveInfo">
+                            <div className="resPrice">{`$${spot.price}`}</div>
+                            <div className="resNight">night</div>
+                        </div>
                         <div className="reviewInfo">
-                        <i className="fa-solid fa-star"/>
-                        {spot.avgStarRating} . {spot.numReviews} reviews
+                            <i className="fa-solid fa-star"/>
+                            {Math.round(rating * 10) / 10}
+                            <div className="dot">
+                                <i class="fa-solid fa-circle"></i>
+                            </div>
+                            {spot.numReviews} reviews
                         </div>
                     </div>
+                    <button className="resButton">
+                        Reserve
+                    </button>
                 </div>
             </div>
+
             <div className="reviewSection">
-                <h2>
-                    <i className="fa-solid fa-star"/>
-                    {spot.avgStarRating} . {spot.numReviews} reviews
+                <h2 className="revHead">
+                    <div className="resIcon">
+                        <i className="fa-solid fa-star"/>
+                    </div>
+                    {Math.round(rating * 10) / 10}
+                    <div className="resDot">
+                        <i class="fa-solid fa-circle"></i>
+                    </div>
+                    <li className="resNum">
+                        {spot.numReviews} reviews
+                    </li>
                 </h2>
                 {user && !(user.id === spot.ownerId) && (
                     <div className="PostReviewModal">
-                        <button onClick={openCreateReviewModal}>Post Your Review</button>
+                        <button className="RevPostButton" onClick={openCreateReviewModal}>Post Your Review</button>
                     </div>
                 )}
             </div>
